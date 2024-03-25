2f70d332e5352302fe9facacf015ebd08abeba2b
Amaan Hafeez <ahafeez7@uwo.ca>
2024-03-25 18:14:38 -0400

    fixed git mess ui FullScreenUI.javan

--- src/FullScreenUI.java
+++ src/FullScreenUI.java
@@ -9,57 +9,112 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * Playground class, here I test out stuff
+ */
 public class FullScreenUI extends JFrame {
-    BufferedImage backgroundImage = null;
-    JLabel backgroundLabel;
 
-    private MapPictureArray picArray;
-
-    private BufferedImage[] backgroundImages; // Array to store background images
+    private JLabel[] countryLabels;
     private int currentBackgroundIndex = 0; // Index of the current background image
+    private int currentCountryIndex = 0;
+    private JLabel backgroundLabel;
+    private JLabel hintLabel;
+    private JPanel countryPanel;
+    private MapPictureArray picArray;
+    private BufferedImage[] backgroundImages;
 
-    public FullScreenUI() {
+    public FullScreenUI() throws IOException {
 
         // Get the size of the screen
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        picArray = new MapPictureArray("Global");
+        setUndecorated(false); // Remove window decorations
+        setResizable(false); // Disable resizing
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Exit application on close
 
-        backgroundImages = picArray.getBackgroundImages();
+        // Set full screen mode
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setPreferredSize(screenSize);
 
-        // Create a JLabel to display the background image
-
-        backgroundLabel = new JLabel(new ImageIcon(backgroundImages[currentBackgroundIndex]));
-        backgroundLabel.setBounds(0, 0, screenSize.width, screenSize.height);
-
-        // Add the background label to the content pane
+        // Set up content pane
+        getContentPane().setLayout(new BorderLayout());
+        BufferedImage backgroundImage = ImageIO.read(new File("Country Data/Maps/bk.jpg"));
+        backgroundLabel = new JLabel(new ImageIcon(backgroundImage));
         getContentPane().add(backgroundLabel);
-        getContentPane().setLayout(null);
-        MyButton testbutton = new MyButton();
-        getContentPane().add(testbutton);
+
+        // Conducted test to ensure country objects can present their own Jlabels succesfully.
+        Country pk = new Country("Pakistan");
+        Country uk = new Country("UnitedKingdom");
+        Country cn = new Country("China");
+        Country ca = new Country("Canada");
+
+        Country[] countries = new Country[]{pk, uk, cn, ca};
+        countryLabels = new JLabel[4];
+        countryLabels[0] = pk.getCountryMap();
+        countryLabels[1] = uk.getCountryMap();
+        countryLabels[2] = cn.getCountryMap();
+        countryLabels[3] = ca.getCountryMap();
 
 
-        testbutton.addActionListener(new ActionListener() {
+//        backgroundLabel.setBounds(0, 0, screenSize.width, screenSize.height);
+
+        // Creating Jlabel for country map
+//        JPanel countryPanel = new JPanel();
+
+        countryPanel = new JPanel(new BorderLayout());
+        countryPanel.setOpaque(false);
+        countryPanel.setPreferredSize(new Dimension(500, 500)); // Adjust height as needed
+        countryPanel.add(backgroundLabel, BorderLayout.CENTER);
+        getContentPane().add(countryPanel, BorderLayout.NORTH);
+
+
+        // Create hint label
+
+        hintLabel = new JLabel("test, first out loop init");
+        hintLabel.setFont(new Font("Arial", Font.PLAIN, 18));
+        hintLabel.setForeground(Color.BLACK);
+
+        // Create hint panel
+        JPanel hintPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        hintPanel.setOpaque(false);
+        hintPanel.setPreferredSize(new Dimension(500, 100));
+        hintPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        hintPanel.add(hintLabel);
+        getContentPane().add(hintPanel, BorderLayout.CENTER);
+
+
+        // Add the panels to the background label
+//        backgroundLabel.add(countryPanel);
+//        backgroundLabel.add(hintPanel);
+
+        // Create buttons panel
+        JPanel buttonsPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        buttonsPanel.setPreferredSize(new Dimension(500, 100));
+        getContentPane().add(buttonsPanel, BorderLayout.SOUTH);
+
+
+        //getContentPane().add(testbutton);
+        JButton testButton = new JButton("Next Country");
+//        JPanel finalCountryPanel = countryPanel;
+        testButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                currentBackgroundIndex = (currentBackgroundIndex + 1) % backgroundImages.length;
-                backgroundLabel.setIcon(new ImageIcon(backgroundImages[currentBackgroundIndex]));
+                currentBackgroundIndex = (currentBackgroundIndex + 1) % countryLabels.length;
+                currentCountryIndex = (currentCountryIndex + 1) % countries.length;
+
+                countryPanel.removeAll();
+                countryPanel.add(countryLabels[currentCountryIndex]);
+                countryPanel.revalidate();
+                countryPanel.repaint();
+
+                String hints = countries[currentCountryIndex].getHints().getText();
+                hintLabel.setText(hints);
             }
-
         });
+        buttonsPanel.add(testButton);
 
-
-        // Add any UI components or game elements here
-        // For now, let's just display a message in the center of the screen
 
         GameSound test = new GameSound("test.wav");
         test.play();
 
-
-        JLabel messageLabel = new JLabel("Fullscreen UI");
-        messageLabel.setFont(new Font("Arial", Font.BOLD, 24));
-        messageLabel.setForeground(Color.WHITE);
-        Dimension labelSize = messageLabel.getPreferredSize();
-        messageLabel.setBounds((screenSize.width - labelSize.width) / 2, (screenSize.height - labelSize.height) / 2, labelSize.width, labelSize.height);
-        backgroundLabel.add(messageLabel);
 
         // Set up key listener to exit fullscreen when ESC key is pressed
         addKeyListener(new KeyAdapter() {
@@ -73,20 +128,11 @@ public class FullScreenUI extends JFrame {
 
         // Set JFrame to be visible
         setVisible(true);
-
         // Request focus so that key events are captured
         requestFocus();
     }
 
-    public static void main(String[] args) {
-        //new FullScreenUI();
-
-        JFrame main = new JFrame();
-        main.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        main.setUndecorated(true);
-        main.setContentPane(new MainMenu(main));
-        main.setVisible(true);
-        main.requestFocus();
-
+    public static void main(String[] args) throws IOException {
+        new FullScreenUI();
     }
 }
