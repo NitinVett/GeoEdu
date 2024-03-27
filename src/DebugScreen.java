ea72ed842fb993134a5d31bfa8b3dfa9394cb7a1
stefanbaggieri <sbaggier@uwo.ca>
2024-03-27 18:43:13 -0400

    Added to debug screen

--- src/DebugScreen.java
+++ src/DebugScreen.java
@@ -10,46 +10,35 @@ public class DebugScreen extends Screen{
     public DebugScreen(JFrame frame) {
         super(frame);
         password = new JTextField("Enter Password", 16);
-        username = new JTextField("Enter Username", 16);
         login = new JButton("LOGIN");
         this.add(password);
-        this.add(username);
         this.add(login);
         login.addActionListener(e -> loginButton());
         setFocusListeners(password,"Enter Password");
-        setFocusListeners(username,"Enter Username");
         repaint();
 
     }
-
     public void setComponents(){
         int width = getWidth();
         int height = getHeight();
         int mainButtonX = width/2 - width/10;
         int mainButtonY =  height/3;
 
-        username.setBounds(mainButtonX, mainButtonY, width/5, height/20);
         password.setBounds(mainButtonX,mainButtonY + height/10,width/5,height/20);
         login.setBounds(mainButtonX,mainButtonY + (height/10)*2,width/5,height/20);
-        username.setFont(new Font("SansSerif", Font.PLAIN, 24));
         password.setFont(new Font("SansSerif", Font.PLAIN, 24));
+        login.setFont(new Font("SansSerif", Font.PLAIN, 24));
         revalidate();
-
-
     }
-
-
-
+    //debugger password 1029384756
     public void loginButton(){
-
-        String pass = CsvHandler.getPassword(username.getText());
-        if(Objects.nonNull(pass) && pass.equals(password.getText())) {
+        String pass = "1029384756";
+        if(pass.equals(password.getText())) {
             swapScreens(new GameMainMenu(frame));
         }else {
-            System.out.println("error");
+            displayErrorMessage("Wrong Password");
         }
     }
-
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
