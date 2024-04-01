2c9542ac0136efaf71bf5c9547c72baec63f41e5
Amaan Hafeez <ahafeez7@uwo.ca>
2024-04-01 04:31:54 -0400

    added escape buttons

--- src/LoginScreen.java
+++ src/LoginScreen.java
@@ -1,14 +1,20 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Objects;
 
-public class LoginScreen extends Screen{
+public class LoginScreen extends Screen {
     JTextField password, username;
-    JButton login;
-    public LoginScreen(FullScreenUI frame,Screen previous) {
+    JButton login, esc;
+
+    public LoginScreen(FullScreenUI frame, Screen previous) {
         super(frame, previous);
+        esc = new JButton();
         password = new JTextField("Enter Password", 16);
         username = new JTextField("Enter Username", 16);
         login = new JButton("LOGIN");
@@ -16,42 +22,57 @@ public class LoginScreen extends Screen{
         this.add(username);
         this.add(login);
         login.addActionListener(e -> loginButton());
-        setFocusListeners(password,"Enter Password");
-        setFocusListeners(username,"Enter Username");
+        setFocusListeners(password, "Enter Password");
+        setFocusListeners(username, "Enter Username");
+
+        BufferedImage escIcon = null;
+        try {
+            escIcon = ImageIO.read(new File("src/escape.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Image resizedEsc = escIcon.getScaledInstance(50, 50, Image.SCALE_SMOOTH);
+        esc.setIcon(new ImageIcon(resizedEsc));
+        esc.addActionListener(e -> exitButton());
+        this.add(esc);
         repaint();
 
     }
 
-    public void setComponents(){
+    public void setComponents() {
         int width = getWidth();
         int height = getHeight();
-        int mainButtonX = width/2 - width/10;
-        int mainButtonY =  height/3;
+        int mainButtonX = width / 2 - width / 10;
+        int mainButtonY = height / 3;
 
-        username.setBounds(mainButtonX, mainButtonY, width/5, height/20);
-        password.setBounds(mainButtonX,mainButtonY + height/10,width/5,height/20);
-        login.setBounds(mainButtonX,mainButtonY + (height/10)*2,width/5,height/20);
+        username.setBounds(mainButtonX, mainButtonY, width / 5, height / 20);
+        password.setBounds(mainButtonX, mainButtonY + height / 10, width / 5, height / 20);
+        login.setBounds(mainButtonX, mainButtonY + (height / 10) * 2, width / 5, height / 20);
         username.setFont(new Font("SansSerif", Font.PLAIN, 24));
         password.setFont(new Font("SansSerif", Font.PLAIN, 24));
+        esc.setBounds(width / 30, height / 22, 50, 50);
+
         revalidate();
 
 
     }
 
 
-
-    public void loginButton(){
+    public void loginButton() {
 
         String pass = CsvHandler.getPassword(username.getText());
-        if(Objects.nonNull(pass) && pass.equals(password.getText())) {
-            Player user = new Player(username.getText(),password.getText());
-            swapScreens(new GameMainMenu(frame,this,user));
+        if (Objects.nonNull(pass) && pass.equals(password.getText())) {
+            Player user = new Player(username.getText(), password.getText());
+            swapScreens(new GameMainMenu(frame, this, user));
 
-        }else {
+        } else {
             displayErrorMessage("Incorrect username/password");
         }
     }
 
+    public void exitButton() {
+        swapScreens(prev);
+    }
 
 
     public void paintComponent(Graphics g) {
--- src/RegisterScreen.java
+++ src/RegisterScreen.java
@@ -1,56 +1,81 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
-public class RegisterScreen extends Screen{
+public class RegisterScreen extends Screen {
     JTextField password, username, password_2;
-    JButton register;
-    public RegisterScreen(FullScreenUI frame,Screen previous) {
-        super(frame,previous);
+    JButton register, esc;
+
+    public RegisterScreen(FullScreenUI frame, Screen previous) {
+        super(frame, previous);
+        esc = new JButton();
         password = new JTextField("Enter Password", 16);
         username = new JTextField("Enter Username", 16);
         password_2 = new JTextField("Re-enter Password", 16);
-        register= new JButton("REGISTER");
+        register = new JButton("REGISTER");
         this.add(password);
         this.add(username);
         this.add(password_2);
         this.add(register);
         register.addActionListener(e -> registerButton());
-        setFocusListeners(password,"Enter Password");
-        setFocusListeners(username,"Enter Username");
-        setFocusListeners(password_2,"Re-enter Password");
+        setFocusListeners(password, "Enter Password");
+        setFocusListeners(username, "Enter Username");
+        setFocusListeners(password_2, "Re-enter Password");
+
+        BufferedImage escIcon = null;
+        try {
+            escIcon = ImageIO.read(new File("src/escape.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Image resizedEsc = escIcon.getScaledInstance(50, 50, Image.SCALE_SMOOTH);
+        esc.setIcon(new ImageIcon(resizedEsc));
+        esc.addActionListener(e -> exitButton());
+        this.add(esc);
+
         repaint();
 
 
     }
-    public void setComponents(){
+
+    public void exitButton() {
+        swapScreens(prev);
+    }
+
+    public void setComponents() {
         int width = getWidth();
         int height = getHeight();
-        int mainButtonX = width/2 - width/10;
-        int mainButtonY =  height/3;
+        int mainButtonX = width / 2 - width / 10;
+        int mainButtonY = height / 3;
 
-        username.setBounds(mainButtonX, mainButtonY, width/5, height/20);
-        password.setBounds(mainButtonX,mainButtonY + height/10,width/5,height/20);
-        password_2.setBounds(mainButtonX,mainButtonY + (height/10)*2,width/5,height/20);
-        register.setBounds(mainButtonX,mainButtonY + (height/10)*3,width/5,height/20);
+        username.setBounds(mainButtonX, mainButtonY, width / 5, height / 20);
+        password.setBounds(mainButtonX, mainButtonY + height / 10, width / 5, height / 20);
+        password_2.setBounds(mainButtonX, mainButtonY + (height / 10) * 2, width / 5, height / 20);
+        register.setBounds(mainButtonX, mainButtonY + (height / 10) * 3, width / 5, height / 20);
         password_2.setFont(new Font("SansSerif", Font.PLAIN, 24));
         username.setFont(new Font("SansSerif", Font.PLAIN, 24));
         password.setFont(new Font("SansSerif", Font.PLAIN, 24));
+        esc.setBounds(width / 30, height/22, 50, 50);
+
         revalidate();
 
 
     }
-    public void registerButton(){
-       if(password.getText().equals(password_2.getText())) {
-           String error = CsvHandler.addUser(username.getText(), password.getText());
-           if(error.equals("APPROVED")) {
-               swapScreens(new MainMenu(frame));
-           }
-           else {
-               displayErrorMessage(error);
-           }
-       }else {
-           displayErrorMessage("You passwords do not match");
-       }
+
+    public void registerButton() {
+        if (password.getText().equals(password_2.getText())) {
+            String error = CsvHandler.addUser(username.getText(), password.getText());
+            if (error.equals("APPROVED")) {
+                swapScreens(new MainMenu(frame));
+            } else {
+                displayErrorMessage(error);
+            }
+        } else {
+            displayErrorMessage("You passwords do not match");
+        }
 
 
     }
