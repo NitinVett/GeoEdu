1b64c284828b6b51365af12e93b3eaacd014d32d
Tianyi Han <than66@uwo.ca>
2024-04-01 01:03:00 -0400

    Tutorial

--- src/TutorialScreen.java
+++ src/TutorialScreen.java
@@ -17,13 +17,21 @@ public class TutorialScreen extends Screen implements KeyListener {
 
     public void displayTutorial(Graphics2D g2D) {
         String[] tutorialText = {
-                "Welcome to the Tutorial!",
+                "Welcome to Geocraft",
                 "",
-                "1. Step One: Do this.",
-                "2. Step Two: Do that.",
-                "3. Step Three: Do something else.",
+                "1. Select a game mode (Global, Continental or Micro-Nations)",
+                "2. Select a game type (Timed, Marathon or Exploration)",
                 "",
-                "Press Enter or Escape to return to the previous screen."
+                "In timed mode, you have 3 minutes to score as many points as you can",
+                "You earn 10 points for a correct answer and lose 5 incorrect answer",
+                "",
+                "In marathon mode, you have 3 lives to score as many points as you can, 10 points per correct answer",
+                "You will lose a live for each wrong answer you get. The game is over when you are out of lives",
+                "",
+                "In both timed and marathon mode, 2 points will be deducted if you choose to view a flag hint",
+                "4 points will be deducted if you choose to view a text hint",
+                "",
+                "Good luck and have fun!"
         };
 
         g2D.setFont(new Font("SansSerif", Font.PLAIN, 24));
